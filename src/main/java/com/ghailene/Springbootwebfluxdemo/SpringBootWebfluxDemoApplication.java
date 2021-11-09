2282dd7287e021f2e110278ce46eed631f469f7e
ghailen <ghailenemark@gmail.com>
2021-11-09 17:51:55 +0100

    my commit

---
+++ src/main/java/com/ghailene/Springbootwebfluxdemo/SpringBootWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ghailene.Springbootwebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxDemoApplication.class, args);
+	}
+
+}
